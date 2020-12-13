1d30f4667a8f6f64f52edb28f59aa636884aa112
sanjar <sanjar@pop-os.localdomain>
2020-12-13 14:58:28 +0500

    must learn sqlalchemy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse, abort
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -8,7 +8,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
 class VidoeModel(db.Model):
-    _id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     views = db.Column(db.Integer, nullable=False)
     likes = db.Column(db.Integer, nullable=False)
@@ -22,16 +22,25 @@ video_put_args.add_argument("views", type=int, help="Views of the vidoe", requir
 video_put_args.add_argument("likes", type=int, help="Likes of the vidoe", required=True)
 
 
+resource_fields = {
+    'id':fields.Integer,
+    'name':fields.String,
+    'views':fields.Integer,
+    'likes':fields.Integer
+}
+
 class Video(Resource):
+    @marshal_with(resource_fields)
     def get(self, video_id):
-        result = VideoModel.query.get(_id=video_id)
+        result = VideoModel.query.filter_by(id=video_id).first()
         return result
         
     def put(self, video_id):
-        abort_if_exist(video_id)
         args = video_put_args.parse_args()
-        videos[video_id] = args
-        return videos[video_id], 201
+        video = VidoeModel(id=video_id, name=args['name'], views=args['views'], likes=args['likes'])
+        db.session.add(video)
+        db.session.commit()
+        return video, 201
         
     def delete(self, video_id):
         abort_if_dont_exist(video_id)
